3030d2fe582d29aca11668ce29bcbb86dc80cc29
Lukas Schwab <lukas.schwab@gmail.com>
2015-08-03 14:32:08 -0700

    Table w. dates

--- tir.py
+++ tir.py
@@ -1,14 +1,34 @@
-import click, sys
+import click, sys, os, datetime
 
 def main():
-	# Ask for each input with click
+    # Ask for each input with click
     url = click.prompt("URL: ")
     name = click.prompt("Title: ")
     author = click.prompt("Author: ")
-	note = click.prompt("Note: ")
+    note = click.prompt("Note: ")
+    today = datetime.date.today().strftime("%B %d, %Y")
 
-	# Render into HTML
-	
-	# Add into file
+    # Render into HTML
+    htmlString = '<tr> <td><a href="'+url+'">'+name+'</a></td> <td>'+author+'</td> <td>'+note+'</td> <td>'+today+'</td> </tr>'
+    print htmlString
 
-	sys.exit(0)
+    # Check if it's a new day
+    # Create the new table if it is
+    # Remove the date column in the table
+
+    # Add into file
+    path = "tir.html"
+    f = open(path, "r")
+    contents = f.readlines()
+    f.close()
+
+    contents.insert(-1, htmlString)
+
+    f = open(path, "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
+    sys.exit(0)
+
+main()
