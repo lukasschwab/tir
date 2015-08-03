7b3cb84cf68a1881b16c4e8344dcb3048804d652
Lukas Schwab <lukas.schwab@gmail.com>
2015-08-03 14:02:11 -0700

    Add user input

---
+++ tir.py
@@ -0,0 +1,14 @@
+import click, sys
+
+def main():
+	# Ask for each input with click
+    url = click.prompt("URL: ")
+    name = click.prompt("Title: ")
+    author = click.prompt("Author: ")
+	note = click.prompt("Note: ")
+
+	# Render into HTML
+	
+	# Add into file
+
+	sys.exit(0)
