fa370607845754ac37c915ac66de188414ce2a8d
Lukas Schwab <lukas.schwab@gmail.com>
2015-09-02 11:42:10 -0700

    Adds separate tables for each new date

--- tir/__main__.py
+++ tir/__main__.py
@@ -6,23 +6,29 @@ def main():
     name = click.prompt("Title")
     author = click.prompt("Author")
     note = click.prompt("Note")
+
+    # Date handling
     today = datetime.date.today().strftime("%B %d, %Y")
 
     # Render into HTML
-    htmlString = '<tr> <td><a href="'+url+'">'+name+'</a></td> <td>'+author+'</td> <td>'+note+'</td> <td>'+today+'</td> </tr>\n'
+    htmlString = '\t<tr> <td><a href="'+url+'">'+name+'</a></td> <td>'+author+'</td> <td>'+note+'</td> <td>'+today+'</td> </tr>\n'
 
-    # Check if it's a new day
-    # Create the new table if it is
-    # Remove the date column in the table
-
-    # Add into file
+    # Read file contents
+    # TODO: MAKE THIS PATH RELATIVE
     path = os.path.expanduser("~/Desktop/Programming Projects/tir/tir.html")
     f = open(path, "r")
     contents = f.readlines()
     f.close()
 
+    if today != contents[0][4:-4]:
+        contents[0] = "<!--"+today+"-->\n"
+        # Create a new table
+        contents += ['\n<h3>'+today+'</h3>\n','<table class="table">\n','\t<tr>\n','\t\t<th>Title</th>\n','\t\t<th>Author</th>\n','\t\t<th>Note</th>\n','\t\t<th>Date</th>\n','\t</tr>\n','</table>']
+
+    # Add new entry
     contents.insert(-1, htmlString)
 
+    # Save changes to file
     f = open(path, "w")
     contents = "".join(contents)
     f.write(contents)
