efe2f1ea2af90afb3f39df69082ffde9c2d6a98d
Lukas Schwab <lukas.schwab@gmail.com>
2021-02-28 16:59:52 -0800

    Simplify push.sh

--- push.sh
+++ push.sh
@@ -3,8 +3,7 @@ cd ~/Programming/tir/
 # If there's a change to tir.html
 if ! git diff-index --quiet HEAD -- index.html tir.xml; then
     # Push any changes in tir.html / tir.xml to github
-    git add tir.html
-    git add tir.xml
+    git add index.html tir.xml
     git commit --quiet -m 'auto tir update'
     git push --quiet
     terminal-notifier -title 'tir' -message 'Automatically updated' -open 'http://lukasschwab.github.io/tir' -sound default
